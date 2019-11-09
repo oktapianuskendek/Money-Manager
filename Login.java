80a439ba760c19adc7b214069a507523e74b6710
KuntoAgung <57562608+KuntoAgung@users.noreply.github.com>
2019-11-09 22:27:17 +0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,13 +1,4 @@
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Felix
- */
 import java.awt.event.KeyEvent;
 import java.sql.*;
 import javax.swing.JFrame;
